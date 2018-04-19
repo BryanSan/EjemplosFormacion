fc2ee7feac926e54dd0eeac2794ca47b1d99ebdc
Bryan Sanchez <BryanSan95@outlook.com>
2018-04-19 12:36:52 +0200

    String Extension IsStringNumberOfTimes

--- EjemplosFormacion.HelperClasess/Extensions/StringExtensions.cs
+++ EjemplosFormacion.HelperClasess/Extensions/StringExtensions.cs
@@ -1,16 +1,23 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace EjemplosFormacion.HelperClasess.Extensions
 {
     public static class StringExtensions
     {
-        public static bool IsValidJson(this string stringValue)
+        /// <summary>
+        /// Extension Method para validar si un string es un JsonValido
+        /// </summary>
+        /// <param name="source">String para validar</param>
+        /// <returns></returns>
+        public static bool IsValidJson(this string source)
         {
             bool isValid = false;
-            if (!string.IsNullOrWhiteSpace(stringValue))
+            if (!string.IsNullOrWhiteSpace(source))
             {
-                string value = stringValue.Trim();
+                string value = source.Trim();
 
                 if ((value.StartsWith("{") && value.EndsWith("}")) || //For object
                     (value.StartsWith("[") && value.EndsWith("]"))) //For array
@@ -28,6 +35,18 @@ namespace EjemplosFormacion.HelperClasess.Extensions
             }
 
             return isValid;
+        }
+
+        /// <summary>
+        /// Extension Method para validar que un string este un numero de veces en un string
+        /// </summary>
+        /// <param name="source">String a testear</param>
+        /// <param name="stringToSearch">String a buscar</param>
+        /// <param name="numberOfTimes">Numero de veces minimas que debe aparecer para estar correcto</param>
+        /// <returns></returns>
+        public static bool IsStringNumberOfTimes(this string source, string stringToSearch, int numberOfTimes)
+        {
+            return Regex.Matches(source, Regex.Escape(stringToSearch)).Count >= numberOfTimes;
         }
     }
 }
