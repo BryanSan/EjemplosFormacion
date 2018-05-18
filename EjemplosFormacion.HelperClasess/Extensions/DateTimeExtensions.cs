cba85c6b4b6981cb78793fe963a4a668191028c3
Bryan Sanchez <BryanSan95@outlook.com>
2018-05-18 09:09:34 +0200

    DateTime Extensions

---
+++ EjemplosFormacion.HelperClasess/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace EjemplosFormacion.HelperClasess.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime FirstDayOfThisWeek(this DateTime dateTime)
+        {
+            var culture = System.Threading.Thread.CurrentThread.CurrentCulture;
+            var diff = dateTime.DayOfWeek - culture.DateTimeFormat.FirstDayOfWeek;
+            if (diff < 0)
+                diff += 7;
+            return dateTime.AddDays(-diff).Date;
+        }
+
+        public static DateTime LastDayOfThisWeek(this DateTime dateTime)
+        {
+            return dateTime.FirstDayOfThisWeek().AddDays(6);
+        }
+
+        public static DateTime FirstDayOfThisMonth(this DateTime dateTime)
+        {
+            return new DateTime(dateTime.Year, dateTime.Month, 1);
+        }
+
+        public static DateTime LastDayOfThisMonth(this DateTime dateTime)
+        {
+            return dateTime.FirstDayOfThisMonth().AddMonths(1).AddDays(-1);
+        }
+
+        public static DateTime FirstDayOfNextMonth(this DateTime dateTime)
+        {
+            return dateTime.FirstDayOfThisMonth().AddMonths(1);
+        }
+    }
+}
