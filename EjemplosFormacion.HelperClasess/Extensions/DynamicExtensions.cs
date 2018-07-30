1fae0a259068c15d3859255836e4df43c8f3efce
Bryan Sanchez <BryanSan95@outlook.com>
2018-07-30 12:32:19 +0200

    Comentarios

--- EjemplosFormacion.HelperClasess/Extensions/DynamicExtensions.cs
+++ EjemplosFormacion.HelperClasess/Extensions/DynamicExtensions.cs
@@ -5,8 +5,16 @@ using System.Xml.Linq;
 
 namespace EjemplosFormacion.HelperClasess.Extensions
 {
+    /// <summary>
+    /// Helper Methods para trabajar con dynamic objects, 
+    /// Como no se puede crear un extension metodo con el "this", lo dejamos como una metodo estatico normal
+    /// </summary>
     public static class DynamicExtensions
     {
+        /// <summary>
+        /// Metodo para convertir un ExpandoObject y sus propiedades a un XmlNode
+        /// https://blogs.msdn.microsoft.com/csharpfaq/2009/09/30/dynamic-in-c-4-0-introducing-the-expandoobject/
+        /// </summary>
         private static XElement ExpandoObjectToXmlNode(dynamic node, String nodeName)
         {
             XElement xmlNode = new XElement(nodeName);
