293eb61b1d5f9e87260d76e6b6ea5e574b7eb7a8
Bryan Sanchez <BryanSan95@outlook.com>
2018-07-30 12:29:02 +0200

    ExpandoObject To Xml Extension

---
+++ EjemplosFormacion.HelperClasess/Extensions/DynamicExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Xml.Linq;
+
+namespace EjemplosFormacion.HelperClasess.Extensions
+{
+    public static class DynamicExtensions
+    {
+        private static XElement ExpandoObjectToXmlNode(dynamic node, String nodeName)
+        {
+            XElement xmlNode = new XElement(nodeName);
+
+            foreach (var property in (IDictionary<String, Object>)node)
+            {
+                if (property.Value.GetType() == typeof(ExpandoObject))
+                {
+                    xmlNode.Add(ExpandoObjectToXmlNode(property.Value, property.Key));
+                }
+                else
+                {
+                    if (property.Value.GetType() == typeof(List<dynamic>))
+                    {
+                        foreach (var element in (List<dynamic>)property.Value)
+                        {
+                            xmlNode.Add(ExpandoObjectToXmlNode(element, property.Key));
+                        }
+                    }
+                    else
+                    {
+                        xmlNode.Add(new XElement(property.Key, property.Value));
+                    }
+                }
+            }
+
+            return xmlNode;
+        }
+    }
+}
