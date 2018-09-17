d39683e460741bcae40fd72fb01050e49e589e78
Bryan Sanchez <BryanSan95@outlook.com>
2018-09-17 15:18:23 +0200

    DigitalCertificateService

---
+++ EjemplosFormacion.HelperClasess/Criptography Helpers/Abstract/IDigitalCertificateService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace EjemplosFormacion.HelperClasess.CriptographyHelpers.Abstract
+{
+    public interface IDigitalCertificateService
+    {
+        X509Certificate2 GetCertificate(string searchValue, X509FindType findType, StoreName storeName, StoreLocation storeLocation, bool validOnly);
+
+        void AddCertificate(X509Certificate2 certificateToAdd, StoreName storeName, StoreLocation storeLocation);
+        void AddCertificate(string certificatePath, StoreName storeName, StoreLocation storeLocation);
+
+        void RemoveCertificate(string searchValue, X509FindType findType, StoreName storeName, StoreLocation storeLocation, bool validOnly);
+
+        byte[] ExportCertificate(string searchValue, X509ContentType contentType, X509FindType findType, StoreName storeName, StoreLocation storeLocation, bool validOnly);
+        byte[] ExportCertificate(string searchValue, X509ContentType contentType, string password, X509FindType findType, StoreName storeName, StoreLocation storeLocation, bool validOnly);
+    }
+}
---
+++ EjemplosFormacion.HelperClasess/Criptography Helpers/DigitalCertificateService.cs
@@ -0,0 +1,105 @@
+﻿using EjemplosFormacion.HelperClasess.CriptographyHelpers.Abstract;
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace EjemplosFormacion.HelperClasess.CriptographyHelpers
+{
+    public class DigitalCertificateService : IDigitalCertificateService
+    {
+        public X509Certificate2 GetCertificate(string searchValue, X509FindType findType, StoreName storeName, StoreLocation storeLocation, bool validOnly)
+        {
+            using (X509Store computerCaStore = new X509Store(storeName, storeLocation))
+            {
+                computerCaStore.Open(OpenFlags.ReadOnly);
+                X509Certificate2 certificate = GetCertificate(searchValue, findType, validOnly, computerCaStore);
+
+                return certificate;
+            }
+        }
+
+        public void AddCertificate(X509Certificate2 certificateToAdd, StoreName storeName, StoreLocation storeLocation)
+        {
+            using (X509Store computerCaStore = new X509Store(storeName, storeLocation))
+            {
+                computerCaStore.Open(OpenFlags.ReadWrite);
+                computerCaStore.Add(certificateToAdd);
+            }
+        }
+
+        public void AddCertificate(string certificatePath, StoreName storeName, StoreLocation storeLocation)
+        {
+            using (X509Certificate2 certificateToAdd = new X509Certificate2(certificatePath))
+            {
+                AddCertificate(certificateToAdd, storeName, storeLocation);
+            }
+        }
+
+        public void RemoveCertificate(string searchValue, X509FindType findType, StoreName storeName, StoreLocation storeLocation, bool validOnly)
+        {
+            using (X509Store certificateStore = new X509Store(storeName, storeLocation))
+            {
+                certificateStore.Open(OpenFlags.ReadWrite);
+
+                X509Certificate2 certificateToDelete = GetCertificate(searchValue, findType, validOnly, certificateStore);
+                if (certificateToDelete != null)
+                {
+                    certificateStore.Remove(certificateToDelete);
+                    certificateToDelete.Dispose();
+                }
+                else
+                {
+                    throw new Exception("Certificado no encontrado");
+                }
+            }
+        }
+
+        public byte[] ExportCertificate(string searchValue, X509ContentType contentType, string password, X509FindType findType, StoreName storeName, StoreLocation storeLocation, bool validOnly)
+        {
+            using (X509Store computerCaStore = new X509Store(storeName, storeLocation))
+            {
+                computerCaStore.Open(OpenFlags.ReadOnly);
+
+                X509Certificate2 certificateToExport = GetCertificate(searchValue, findType, validOnly, computerCaStore);
+                if (certificateToExport != null)
+                {
+                    byte[] certificateBytes = null;
+                    if (!string.IsNullOrWhiteSpace(password))
+                    {
+                        certificateBytes = certificateToExport.Export(contentType, password);
+                    }
+                    else
+                    {
+                        certificateBytes = certificateToExport.Export(contentType);
+                    }
+
+                    return certificateBytes;
+                }
+                else
+                {
+                    throw new Exception("Certificado no encontrado");
+                }
+            }
+        }
+
+        public byte[] ExportCertificate(string searchValue, X509ContentType contentType, X509FindType findType, StoreName storeName, StoreLocation storeLocation, bool validOnly)
+        {
+            byte[] certificateBytes = ExportCertificate(searchValue, contentType, null, findType, storeName, storeLocation, validOnly);
+
+            return certificateBytes;
+        }
+
+        static X509Certificate2 GetCertificate(string searchValue, X509FindType findType, bool validOnly, X509Store computerCaStore)
+        {
+            X509Certificate2Collection certificatesInStore = computerCaStore.Certificates.Find(findType, searchValue, validOnly);
+
+            if (certificatesInStore.Count > 0)
+            {
+                return certificatesInStore[0];
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
