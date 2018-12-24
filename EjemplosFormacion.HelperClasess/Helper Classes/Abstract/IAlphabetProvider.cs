ec86b34a93e715da525c640fc913aac96d5522f1
Bryan Sanchez <Bsanchez@pasiona.com>
2018-12-24 14:31:44 +0100

    Alphabet Provider

---
+++ EjemplosFormacion.HelperClasess/Helper Classes/Abstract/IAlphabetProvider.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace EjemplosFormacion.HelperClasess.HelperClasses.Abstract
+{
+    public interface IAlphabetProvider
+    {
+        IList<string> GetAlphabet(bool lowerCase);
+    }
+}
---
+++ EjemplosFormacion.HelperClasess/Helper Classes/AlphabetProvider.cs
@@ -0,0 +1,17 @@
+﻿using EjemplosFormacion.HelperClasess.HelperClasses.Abstract;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EjemplosFormacion.HelperClasess.HelperClasses
+{
+    public class AlphabetProvider : IAlphabetProvider
+    {
+        public IList<string> GetAlphabet(bool lowerCase)
+        {
+            char A = lowerCase ? 'a' : 'A';
+            char Z = lowerCase ? 'z' : 'Z';
+
+            return Enumerable.Range(A, Z - A + 1).Select(x => ((char)x).ToString()).ToList();
+        }
+    }
+}
